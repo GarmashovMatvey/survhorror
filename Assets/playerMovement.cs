af7414f42d28f13a9f8c0f5367bfd1f1fd6ea0fd
negr krch <garmashov.matvey@yandex.ru>
2023-07-16 14:46:19 +0700

    added player movement

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -1,18 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float moveSpeed;
+    private Rigidbody2D _rb;
+    private float horInput, verInput;
+    
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horInput = Input.GetAxisRaw("Horizontal");
+        verInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = new Vector2(horInput,verInput) * moveSpeed;
     }
 }
